51eb7b040870b8c16dd3d4fb9b59c30674f7f2f8
Darren Barklie <darrenbarklie@gmail.com>
2022-01-30 21:15:29 +0000

    Build memoized selector

--- src/features/cart/CartLink.tsx
+++ src/features/cart/CartLink.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { getNumItems } from "./cartSlice";
+import { getNumItems, getMemoizedNumItems } from "./cartSlice";
 import { useAppSelector } from "../../app/hooks";
 import styles from "./CartLink.module.css";
 
 export function CartLink() {
-  const numItems = useAppSelector(getNumItems);
+  const numItems = useAppSelector(getMemoizedNumItems);
   return (
     <Link to="/cart" className={styles.link}>
       <span className={styles.text}>
--- src/features/cart/cartSlice.ts
+++ src/features/cart/cartSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, createSelector, PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from "../../app/store";
 
 export interface CartState {
@@ -29,9 +29,22 @@ export default cartSlice.reducer;
 
 // Selector function
 export function getNumItems(state: RootState) {
+  console.log("Calling numItems");
   let numItems = 0;
   for (let id in state.cart.items) {
     numItems += state.cart.items[id];
   }
   return numItems;
 }
+
+export const getMemoizedNumItems = createSelector(
+  (state: RootState) => state.cart.items,
+  (items) => {
+    console.log("Calling getMemoizedNumItems");
+    let numItems = 0;
+    for (let id in items) {
+      numItems += items[id];
+    }
+    return numItems;
+  }
+);
